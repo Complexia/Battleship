85e71e6611d7bee4b28368bcca7b41ae13e67e25
Roman Lobanov <s3399752@b01409023-23.local>
2018-09-27 16:05:54 +1000

    random player started

--- src/player/RandomGuessPlayer.java
+++ src/player/RandomGuessPlayer.java
@@ -1,6 +1,12 @@
 package player;
 
 import java.util.Scanner;
+
+import ship.AircraftCarrier;
+import ship.Cruiser;
+import ship.Frigate;
+import ship.PatrolCraft;
+import ship.Submarine;
 import world.World;
 
 /**
@@ -10,26 +16,77 @@ import world.World;
  * @author Youhan Xia, Jeffrey Chan
  */
 public class RandomGuessPlayer implements Player{
-
+	World world;
+	private int AircraftCarrier = 5;
+	private int Frigate = 4;
+	private int Submarine = 3;
+	private int Cruiser = 4;
+	private int PatrolCraft = 2;
     @Override
     public void initialisePlayer(World world) {
-        // To be implemented.
+        this.world = world;
+    	
     } // end of initialisePlayer()
 
     @Override
     public Answer getAnswer(Guess guess) {
-        // To be implemented.
+    	Answer answer = new Answer();
+    	for(int i=0;i<world.shipLocations.size();i++) {
+    		for(int j = 0;j<world.shipLocations.get(i).coordinates.size();j++) {
+    			if(world.shipLocations.get(i).coordinates.get(j).column == guess.column && world.shipLocations.get(i).coordinates.get(j).row == guess.row) {
+        			
+        			answer.isHit = true;
+        			
+        			String shipName = world.shipLocations.get(i).ship.name();
+                    switch (shipName) {
+                        case "AircraftCarrier":
+                            AircraftCarrier--;
+                            break;
+                        case "Frigate":
+                            Frigate--;
+                            break;
+                        case "Submarine":
+                            Submarine--;
+                            break;
+                        case "Cruiser":
+                            Cruiser--;
+                            break;
+                        case "PatrolCraft":
+                            PatrolCraft--;
+                            break;
+                        
+                    }
+        			
+        		}
+    			if(AircraftCarrier == 0) {
+    				answer.shipSunk = new AircraftCarrier();
+    			}
+    			if(Frigate == 0) {
+    				answer.shipSunk = new Frigate();
+    			}
+    			if(Submarine == 0) {
+    				answer.shipSunk = new Submarine();
+    			}
+    			if(Cruiser == 0) {
+    				answer.shipSunk = new Cruiser();
+    			}
+    			if(PatrolCraft == 0) {
+    				answer.shipSunk = new PatrolCraft();
+    			}
+    		}
+    		
+    	}
 
-        // dummy return
-        return null;
+        
+    	return answer;
     } // end of getAnswer()
 
 
     @Override
     public Guess makeGuess() {
-        // To be implemented.
+        
 
-        // dummy return
+        
         return null;
     } // end of makeGuess()
 
